562e94465938c26127401b1ce819cb915b9e36d5
John Daryl Homecillo <homecillojohndaryl@yahoo.com>
2021-01-29 18:44:09 +0800

    Update crud.php

--- 201880126/crud.php
+++ 201880126/crud.php
@@ -1,19 +1,27 @@
 <?php
-    
+
     $db = mysqli_connect('localhost', 'root', '', 'homecillo');
 
     $id = 0;
-    $fname = "";
-    $lname = "";
-    $program = "";
+    $iso = "";
+    $name = "";
+    $nicename = "";
+    $iso3 = "";
+    $numcode = "";
+    $phonecode = "";
+    $created_at = "";
     $edit_state = false;
 
     if (isset($_POST['save-btn'])) {
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $program = $_POST['program'];
+        $iso = $_POST['iso'];
+        $name = $_POST['name'];
+        $nicename = $_POST['nicename'];
+        $iso3 = $_POST['iso3'];
+        $numcode = $_POST['numcode'];
+        $created_at = $_POST['created_at'];
+        $edit_state = false;
 
-        $query = "INSERT INTO crud (fname, lname, program) VALUES ('$fname', '$lname', '$program')";
+        $query = "INSERT INTO country (iso, name, nicename, iso3, numcode, phonecode, created_at) VALUES ('$iso', '$name', '$nicename', '$iso3', '$numcode', '$phonecode', '$created_at')";
         mysqli_query($db, $query);
         $_SESSION['message'] = "Data saved to database.";
         $_SESSION['alert-class'] = "alert-success";
@@ -21,12 +29,15 @@
     }
 
     if (isset($_POST['update-btn'])) {
-        $id = mysqli_real_escape_string($db, $_POST['id']);
-        $fname = mysqli_real_escape_string($db, $_POST['fname']);
-        $lname = mysqli_real_escape_string($db, $_POST['lname']);
-        $program = mysqli_real_escape_string($db, $_POST['program']);
+        $iso = mysqli_real_escape_string($db, $_POST['iso']);
+        $name = mysqli_real_escape_string($db, $_POST['name']);
+        $nicename = mysqli_real_escape_string($db, $_POST['nicename']);
+        $iso3 = mysqli_real_escape_string($db, $_POST['iso3']);
+        $numcode = mysqli_real_escape_string($db, $_POST['numcode']);
+        $phonecode = mysqli_real_escape_string($db, $_POST['phonecode']);
+        $created_at = mysqli_real_escape_string($db, $_POST['created_at']);
 
-        mysqli_query($db, "UPDATE crud SET fname='$fname', lname='$lname', program='$program' where id='$id'");
+        mysqli_query($db, "UPDATE country SET iso='$iso', name='$name', nicename='$nicename', iso3='$iso3', numcode='$numcode', phonecode='$phonecode' where id='$id'");
         $_SESSION['message'] = 'Data updated.';
         $_SESSION['alert-class'] = "alert-success";
         header('location: index.php');
@@ -34,11 +45,11 @@
 
     if (isset($_GET['delete-btn'])) {
         $id = $_GET['delete-btn'];
-        mysqli_query($db, "DELETE FROM crud WHERE id='$id'");
+        mysqli_query($db, "DELETE FROM country WHERE id='$id'");
         $_SESSION['message'] = "Data deleted from database.";
         header('location: index.php');
     }
 
-    $results = mysqli_query($db, "SELECT * FROM crud");
+    $results = mysqli_query($db, "SELECT * FROM country");
     
 ?>
